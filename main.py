c8eb1128b69a927c04205e6ba0656ed861081fa7
Chelsea Houston <chelseahouston29@gmail.com>
2020-09-24 13:29:31 +0000

    typo error

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def calc():
   try:
     b = int(b)
   except ValueError:
-    print("Incorrect value.Resarting Calculator...")
+    print("Incorrect value. Resarting Calculator...")
     calc()
     run()
-  print("To Add these numbers together, type A")
-  print("To Subtract your first number from the second, type B")
-  print("To Multiply these numbers together, type C")
-  print("To Divide the first number by the second, type D")
-  print("To times a to the power of b, type E")
-  value = input("Type your letter (in capitals) here: ")
+  print("To Add these numbers together, type the letter A")
+  print("To Subtract your first number from the second, type the letter B")
+  print("To Multiply these numbers together, type the letter C")
+  print("To Divide the first number by the second, type the letter D")
+  print("To times a to the power of b, type the letter E")
+  value = input("Type your chosen letter (in capitals) here: ")
   if value == "A":
     print(a + b)
   elif value == "B":
@@ -30,7 +30,7 @@ def calc():
   elif value == "E":
     print(a**b)
   else: 
-    print("Incorrect value")
+    print("Error: Incorrect value")
 
 def run():
   runagain = input("Run again? (Y/N): ")
@@ -40,7 +40,7 @@ def run():
   elif runagain == "N":
     print("Thank you for using Chelsea's Calculator. Have a good day.")
   else:
-    print("Incorrect Value.")
+    print("Error: Incorrect Value.")
     run()
 calc()
 run()
